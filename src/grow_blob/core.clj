0636cbc8d57f18716cfd5ef40393b0ceea9a517f
Christy O'Reilly <christy@oreills.co.uk>
2013-04-11 23:25:08 +0100

    Play around with slick graphics

--- src/grow_blob/core.clj
+++ src/grow_blob/core.clj
@@ -3,16 +3,54 @@
     (org.newdawn.slick AppGameContainer BasicGame Sound Font Graphics GameContainer))
   (:gen-class))
 
+(def ^:dynamic *slick-thread* (atom nil))
+
+(def ^:dynamic *text* (atom "=)"))
+(def ^:dynamic *x* (atom 0))
+(def ^:dynamic *xden* (atom 4))
+
+(defn update [container delta]
+  (swap! *x* + (/ delta @*xden*)))
+
+(defn render [container graphics]
+  (.drawString graphics @*text* @*x* 100))
+
+(def key-fns
+  {
+   57 #(reset! *x* 0)
+   203 #(swap! *xden* * 2)
+   205 #(swap! *xden* / 2)
+   200 #(reset! *text* "=D")
+   208 #(reset! *text* "=)")
+   })
+
+(defn key-pressed [k c]
+  ((get
+     key-fns k
+     #(println "No op defined for" k))))
+
 (def simple-test
   (proxy [BasicGame] ["SimpleTest"]
     (init [container])
-    (update [container delta])
+    (update [container delta]
+      (update container delta))
     (render [container graphics]
-      (.drawString graphics "Hellow Slick world!" 0 100)
-      )))
+      (render container graphics))
+    (keyPressed [k c]
+      (key-pressed k c))
+    ))
+
+(defn start-game []
+  (reset!
+    *slick-thread*
+    (future
+      (doto
+        (new AppGameContainer simple-test)
+        (.setTargetFrameRate 60)
+        (.setAlwaysRender true)
+        (.start)
+        ))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (.start (new AppGameContainer simple-test))
-  )
+  (start-game))
