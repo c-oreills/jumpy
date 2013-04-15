88a3391f995318dc168688df78f452de5ebb664b
Christy O'Reilly <christy@oreills.co.uk>
2013-04-15 13:20:20 +0100

    Basic (bad) physics

--- src/grow_blob/core.clj
+++ src/grow_blob/core.clj
@@ -1,5 +1,5 @@
 (ns grow-blob.core
-  (:import 
+  (:import
     (org.newdawn.slick AppGameContainer BasicGame Color Sound Font Graphics GameContainer SpriteSheet)
     )
   (:gen-class))
@@ -8,44 +8,86 @@
 
 (def ^:dynamic *text* (atom "=)"))
 
+(defn new-player [id]
+  {:id id :type :frog
+   :x 0 :y 0 :dx 0 :dy 0 :grounded true
+   :key-left false :key-right false
+   :key-up false :key-down false})
+
 (def ^:dynamic *world* (atom {:players {}}))
 
 (def speed 4)
 
-(defn new-player [id]
-  {:id id :type :frog
-   :x 0 :y 0 :dx 0 :dy 0
-   :key-left false :key-right false
-   :key-up false :key-down false})
+(def jump-init-dy -5)
+(def jump-cont-dy -1)
+(def gravity-dy 2)
+
+(defn handle-player-keys [player]
+  (conj
+    player
+    {:dx (+
+          (if (player :key-left) -1 0)
+          (if (player :key-right) 1 0))
+     :dy (+
+          (player :dy)
+          gravity-dy
+          (or
+            (if (player :key-up)
+              (if (player :grounded)
+                jump-init-dy
+                (if (neg? (player :dy))
+                  jump-cont-dy)))
+            0))}))
+
+(defn update-player-pos [player]
+  (let [player (handle-player-keys player)]
+    (conj
+      player
+      {:x (+ (player :x) (player :dx))
+       :y (min
+            (+ (player :y) (player :dy))
+            400)})))
 
 (defn update [container delta]
-  )
+  (swap! *world*
+         (fn [w]
+           (conj
+             w
+             {:players (into {} (for [[id p] (w :players)] [id (update-player-pos p)]))}
+  ))))
 
 (defn render [container graphics]
-  (let [ss (new SpriteSheet "res/animals.png" 32 32)]
-    (.drawImage graphics (.getSubImage ss 0 0) 10 10))
-  )
+  (let [w @*world*
+        ss (new SpriteSheet "res/animals.png" 32 32)]
+    (doseq [[_ p] (w :players)]
+      (.drawImage graphics (.getSubImage ss 0 0) (p :x) (p :y)))))
 
 (defn set-player-key [player-id key value]
-  (swap! *world* #(assoc-in % [:players player-id key] value)))
+  (swap! *world* (fn [w]
+                   (let [p
+                         (assoc
+                           (get-in w [:players player-id]
+                                   (new-player player-id))
+                           key value)]
+                     (assoc-in w [:players player-id] p)))))
 
-(defn set-player-key-vec [player-id key]
+(defn set-player-key-fns-vec [player-id key]
   (vec
     (for [b [true false]]
       (partial set-player-key player-id key b))))
 
 (defn set-player-key-fns [player-id l r u d]
-  [for [[d v] [
-    l (set-player-key-vec :key-left)
-     r (set-player-key-vec :key-right)
-     u (set-player-key-vec :key-up)
-     d (set-player-key-vec :key-down)
+  {
+   l (set-player-key-fns-vec player-id :key-left)
+   r (set-player-key-fns-vec player-id :key-right)
+   u (set-player-key-fns-vec player-id :key-up)
+   d (set-player-key-fns-vec player-id :key-down)})
 
 (def key-fns
   (conj
-    (set-player-key-fns 1 205 203 200 208)
-    (set-player-key-fns 2 31 30 17 19)
-    (set-player-key-fns 3 77 75 72 76)))
+    (set-player-key-fns 1 203 205 200 208)
+    (set-player-key-fns 2 30 31 17 19)
+    (set-player-key-fns 3 75 77 72 76)))
 
 (defn key-handler [k c t]
   ((or
