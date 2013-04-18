ed5fca95e4713087a9bd2d4510c4cbf9e5b23421
Christy O'Reilly <christy@oreills.co.uk>
2013-04-18 13:03:46 +0100

    Jumpy

--- src/grow_blob/core.clj
+++ src/grow_blob/core.clj
@@ -9,36 +9,49 @@
 (def ^:dynamic *text* (atom "=)"))
 
 (defn new-player [id]
-  {:id id :type :frog :heading :left :tick 0
+  {:id id
+   :type :frog :heading :left :frame :still :tick 0
    :x 0 :y 0 :dx 0 :dy 0
    :key-left false :key-right false
    :key-up false :key-down false})
 
 (def ^:dynamic *world* (atom {:players {}}))
 
-(def speed 4)
+(def speed-x 2)
 (def block-size 32)
 
 (def jump-init-dy -15)
 (def jump-cont-dy -2)
 (def gravity-dy 3)
 
-(def per-frame 100)
+(def per-frame 150)
 
 (def animal-map
   {:cat [0 0] :dove-w [3 0] :dove-f [6 0] :rat [9 0]
    :chick [0 4] :chicken [3 4] :frog [6 4] :rabbit [9 4]})
 
+(def loop-types
+  (let [alternating [:start :end]
+        cycle [:start :end :still]]
+    {:cat alternating :dove-w alternating :dove-f alternating :rat alternating
+     :chick cycle :chicken alternating :frog cycle :rabbit cycle}))
+
+(def frames [:start :still :end])
+
 (defn grounded? [player]
   (= 400 (player :y)))
 
 (defn update-player-velocity [player delta]
-  (letfn [(new-tick [] (mod
-                         (+ (player :tick) delta)
-                         (* 3 per-frame)))]
+  (let [loop-type (loop-types (player :type))
+        new-tick  (mod
+                    (+ (player :tick) delta)
+                    (* (count loop-type) per-frame))
+        grounded (grounded? player)
+        new-frame (loop-type (quot new-tick per-frame))
+        ]
     (conj
       player
-      {:dy (if (grounded? player)
+      {:dy (if grounded
              (if (player :key-up)
                jump-init-dy
                0)
@@ -51,11 +64,13 @@
                 jump-cont-dy
                 0)))}
       (if (player :key-down) [:type (rand-nth (keys animal-map))])
-      (cond
-        (and (player :key-left) (player :key-right)) {:dx 0}
-        (player :key-left) {:dx -1 :heading :left :tick (new-tick)}
-        (player :key-right) {:dx 1 :heading :right :tick (new-tick)}
-        :else {:dx 0 :tick per-frame})
+      (case [(player :key-left) (player :key-right)]
+        [true true] {:dx 0}
+        [true false] {:dx (- speed-x) :heading :left :frame new-frame :tick new-tick}
+        [false true] {:dx speed-x :heading :right :frame new-frame :tick new-tick}
+        [false false] {:dx 0 :frame :still :tick 0})
+      (if (not grounded)
+        {:frame (if (neg? (player :dy)) :start :end)})
       )))
 
 (defn update-player-pos [player]
@@ -81,13 +96,13 @@
   (def ss (new SpriteSheet "res/animals.png" block-size block-size))
   )
 
-(defn get-sprite [^SpriteSheet ss animal dir n]
+(defn get-sprite [^SpriteSheet ss animal dir frame]
   (let [dirs-map
         (into {} (map-indexed (comp vec reverse vector)
                               [:down :left :right :up]))
         [x y] (apply map + [(animal-map animal)
                             [0 (dirs-map dir)]
-                            [n 0]])]
+                            [(.indexOf frames frame) 0]])]
     (.getSubImage ss x y)))
 
 (defn render [container graphics]
@@ -97,7 +112,7 @@
                   (get-sprite ss
                               (p :type)
                               (p :heading)
-                              (quot (p :tick) per-frame))
+                              (p :frame))
                   (p :x)
                   (p :y)))))
 
